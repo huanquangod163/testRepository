46072b8377cd4437e57c2cb4ec310b7d4a6ab172
huanquangod@163.com <huanquangod@163.com>
2021-08-04 22:12:53 +0800

    提交testApp3

--- untitled/src/testApp.java
+++ untitled/src/testApp.java
@@ -3,6 +3,7 @@ public class testApp {
     public static void main(String[] args) {
         System.out.println("hello world!");
         System.out.println("hello 2");
+        System.out.println("hello3");
     }
 
 }
